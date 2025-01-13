27370b411f1a58e7c0627b7a1ce857e5f58c62ff
dhanushm18 <dmdhanushm17@gmail.com>
2025-01-13 21:43:42 +0530

    Update Restuarant.jsx

--- client/src/components/getrestuarant/Restuarant.jsx
+++ client/src/components/getrestuarant/Restuarant.jsx
@@ -10,7 +10,7 @@ const Restaurant = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get("http://localhost:8000/api/restaurants");
+        const response = await axios.get("https://restuarant-crud-operation-2.onrender.com/api/restaurants");
         setRestaurants(response.data);
       } catch (error) {
         console.error("Error fetching restaurants:", error);
