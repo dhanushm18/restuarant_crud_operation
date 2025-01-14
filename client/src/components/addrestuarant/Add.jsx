f6f6543e0fa5e5c8a299f66499c1fd35bab8c70c
dhanushm18 <dmdhanushm17@gmail.com>
2025-01-14 14:40:00 +0530

    Update Add.jsx

--- client/src/components/addrestuarant/Add.jsx
+++ client/src/components/addrestuarant/Add.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import toast from "react-hot-toast";
 import { Link, useNavigate } from "react-router-dom";
+import './Add.css'; 
 
 const Add = () => {
   const initialRestaurantState = {
