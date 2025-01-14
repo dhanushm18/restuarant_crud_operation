d35e106e9b38259c657d60b802dc73a5e799e332
dhanushm18 <dmdhanushm17@gmail.com>
2025-01-14 14:56:22 +0530

    Update Restuarant.jsx

--- client/src/components/getrestuarant/Restuarant.jsx
+++ client/src/components/getrestuarant/Restuarant.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import toast from "react-hot-toast";
 import { Link } from "react-router-dom";
-import 'client/src/components/getrestuarant/Restaurant.css'; 
+import './Restaurant.css'; 
 
 
 const Restaurant = () => {
