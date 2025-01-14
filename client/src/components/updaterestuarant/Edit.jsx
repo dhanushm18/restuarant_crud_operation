978ee8a0413198c45e3b3ef40a1b67fa7a0692d1
dhanushm18 <dmdhanushm17@gmail.com>
2025-01-14 14:55:29 +0530

    Update Edit.jsx

--- client/src/components/updaterestuarant/Edit.jsx
+++ client/src/components/updaterestuarant/Edit.jsx
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 import toast from "react-hot-toast";
-import 'client/src/components/updaterestuarant/Edit.css'; 
+import './Edit.css';
+
+ 
 
 const EditRestaurant = () => {
   const defaultRestaurant = {
